6da897742d67a6f85f10bb4c7ef65f134869cf79
aalmansa-13 <aalmansa@gmail.com>
2019-01-23 13:33:20 +0100

    [AA] Some clean up

--- src/main/java/cabify/repository/promotions/PromotionsRepository.java
+++ src/main/java/cabify/repository/promotions/PromotionsRepository.java
@@ -4,16 +4,17 @@ import cabify.model.promotions.BulkPromotion;
 import cabify.model.promotions.Promotion;
 import cabify.model.promotions.TwoForOnePromotion;
 
-import java.util.Arrays;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class PromotionsRepository {
 
     public static Promotion VOUCHER2x1 = new TwoForOnePromotion("VOUCHER");
     public static Promotion TSHIRTBULK = new BulkPromotion("TSHIRT", 3, 19);
-
+    public static List<Promotion> itemLevelPromotions = asList(VOUCHER2x1, TSHIRTBULK);
 
     public List<Promotion> getItemLevelPromotions() {
-        return Arrays.asList(VOUCHER2x1, TSHIRTBULK);
+        return itemLevelPromotions;
     }
 }
